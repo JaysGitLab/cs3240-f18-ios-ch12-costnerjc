a932e647280eca3b9d88093757154b3f228ff3d1
Chase Costner <chase@Chases-MacBook-Pro.local>
2018-10-31 20:50:15 -0400

    implemented settings view interface

--- Bridge Control/SecondViewController.swift
+++ Bridge Control/SecondViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet var engineSwitch:UISwitch!
+    @IBOutlet var warpFactorSlider:UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func onEngineSwitchTapped(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(engineSwitch.isOn, forKey: warpDriveKey)
+    }
+    
+    @IBAction func onWarpSliderDragged(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(warpFactorSlider.value, forKey: warpFactorKey)
+    }
+    @IBAction func onSettingsButtonTapped(_ sender: Any) {
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+    }
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        engineSwitch.isOn = defaults.bool(forKey: warpDriveKey)
+        warpFactorSlider.value = defaults.float(forKey: warpFactorKey)
+    }
 }
 
